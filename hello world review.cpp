77d97f96cfd05c903e988ea9373d7369c2597f8b
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-08-13 11:33:39 -0700

    Add files via upload

---
+++ hello world review.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World reveiw!" << endl;
+    int a = 22;
+    int b = 23;
+
+int sum = a + b;
+std::cout << sum << std::endl;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.cout << "Hello World";
+
+
+    return 0;
+}
