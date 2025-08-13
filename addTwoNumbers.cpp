d882037b88365dab94a2445fcffb34b332cc06eb
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-08-13 12:24:28 -0700

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,20 @@
+// created by Bryza Arreola Vazquez
+// addTwonumbers.cpp/
+
+#include <iostream>
+using namespace std;
+
+// Created a very important function named main
+//This is where your program starts
+int main() {
+
+// creat variables needed for this program.
+
+
+int sumOfNum = 0;
+int num1 = 0;
+int sum2 = 0;
+    cout
+
+    return 0;
+}
