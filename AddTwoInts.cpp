09bfdbe9566b8b656281129ca4d8cef01a283f18
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-08-18 12:16:48 -0700

    Add files via upload

---
+++ AddTwoInts.cpp
@@ -0,0 +1,45 @@
+//addTwoInts.cpp
+//BAV 8/18/2025
+//https://www.w3schools.com/cpp/cpp_user_input.asp
+// this program will get three ints from the users and output the largest 3
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello and Welcome to My largest of three ints program.\n" ;
+    int num1;
+    int num2;
+    int num3;
+    int sumOfInineints;
+
+    //I want to get 3 ints from the user
+
+    //initialize all my variables
+    //assign a zero value tomy variables
+    num1 = 0;
+
+    //get num1
+    cout <<"\n please enter a value for num1: ";
+    cin >> num1;
+    cout << "\n you enter " << num1 << "for num1";
+    //get num2
+    cout <<"\n please enter a value for num1: ";
+    cin >> num1;
+    cout << "\n you enter " << num1 << "for num1";
+    //get num3
+    cout <<"\n please enter a value for num1: ";
+    cin >> num1;
+    cout << "\n you enter " << num1 << "for num1";
+
+
+    //compare num1 and num2
+if (num1 > num1) {
+    cout << num1 << "is grater than" << num2;
+    else {
+        cout << num2 << " is grater than" << num1;
+
+    }
+}
+    return 0;
+}
