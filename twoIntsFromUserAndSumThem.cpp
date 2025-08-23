2537c65eec389b75f63a40cbf748755f3869a2b0
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-08-23 11:37:33 -0700

    Add files via upload

---
+++ twoIntsFromUserAndSumThem.cpp
@@ -0,0 +1,24 @@
+//Hello professor Mohle This is the last code I was able to do for the modual one activites. only one of the videos was working for me in the textbook and all of my other codes I submitted I figured out on my own I do hope they are correct, Thank you have a great day,
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1;
+    int num2;
+    int sum;
+
+
+    cout << "Please Enter First Number : " << endl;
+    cin >> num1;
+
+
+    cout << "Please Enter First Number : " << endl;
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Total For The Number is : " << sum << endl;
+
+    return 0;
+}
