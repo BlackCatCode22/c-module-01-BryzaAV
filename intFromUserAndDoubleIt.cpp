5301fb74f5a8de27896008d3b3588fb98e505ac8
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-08-23 11:25:23 -0700

    Add files via upload

---
+++ intFromUserAndDoubleIt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1;
+
+
+    cout << "Please Enter A Number : " << endl;
+    cin >> num1;
+
+    int doubled = num1 * 2;
+
+    cout << "The doubled value is: " << doubled << endl;
+
+
+
+    return 0;
+}
