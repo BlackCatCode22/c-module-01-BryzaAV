660c67e2e571787a4b5cf3c9e6b9ee10d1450bc1
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-08-23 11:27:44 -0700

    Add files via upload

---
+++ intFromUserAndtripleIt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1;
+
+
+    cout << "Please Enter A Number : " << endl;
+    cin >> num1;
+
+    int triple = num1 * 3;
+
+    cout << "The doubled value is: " << triple << endl;
+
+
+
+    return 0;
+}
