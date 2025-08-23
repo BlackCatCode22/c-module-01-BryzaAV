7990d3fc3c8b914ca6ccafb8b37fc16e188c09de
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-08-23 11:12:50 -0700

    Add files via upload

---
+++ usersNameandFriendlyGreeting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name;
+
+
+    cout << "Please Enter Your Name : " << endl;
+    cin >> name;
+
+    cout << "Your Name is : " + name <<endl;
+
+    cout << "Its Very Nice To Meet You!" ;
+
+
+    return 0;
+}
